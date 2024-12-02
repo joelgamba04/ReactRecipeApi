53d9325436c3b02821d04260cf3e026274da5b0e
Joel Gamba <joelgamba04@gmail.com>
2024-12-02 08:56:12 +0800

    Added routing

--- recipeapi/src/App.jsx
+++ recipeapi/src/App.jsx
@@ -1,10 +1,23 @@
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import RecipeList from "./components/RecipeList";
+import CreateRecipe from "./components/CreateRecipe";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <>
       <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/create" element={<CreateRecipe />} />
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
